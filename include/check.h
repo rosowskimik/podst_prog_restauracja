5e3345f619c65b4090b4da2b9658c36ae7c57297
Mikołaj Rosowski <49301780+rosowskimik@users.noreply.github.com>
2022-01-25 13:26:51 +0100

    fix: add header guards to check

--- include/check.h
+++ include/check.h
@@ -1,3 +1,6 @@
+#ifndef __CHECK_H__
+#define __CHECK_H__
+
 #include <filesystem>
 
 namespace fs = std::filesystem;
@@ -9,3 +12,4 @@ void check_path_not_empty(const fs::path& to_check);
 void check_path_is_file(const fs::path& to_check);
 
 void check_path_is_directory(const fs::path& to_check);
+#endif // __CHECK_H__
